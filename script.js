fa2ac6e2f6b2c75b365d7f877a3c057b4db558c3
pedrofelip <pedrofelipbarros@gmail.com>
2022-05-23 16:50:05 -0300

    finalizado sem resposividade

--- script.js
+++ script.js
@@ -10,30 +10,64 @@ function mostrarOcultarSenha(idIcone, idInput) {
     }
   }
 
+  function mostrarOcultarModal(idmodal) {
+    var modal_saida = document.getElementById(`${idmodal}`);
+    if (modal_saida.style.display ="none") {
+      modal_saida.style.display ="block"
+    }
+  }
+
+  function mostrarPopUp(idPopUp) {
+    var popUp = document.getElementById(`${idPopUp}`);
+    if (popUp.style.display ="none") {
+      popUp.style.display ="block"
+    }
+  }
+
+  function fecharPopUp(idPopUp) {
+    var popUp = document.getElementById(`${idPopUp}`);
+    if (popUp.style.display ="block") {
+      popUp.style.display ="none"
+    }
+  }
+
+
+  var modal_sair = document.getElementById("fundo_modal");
+  
+  var fechar_modal_sair = document.getElementsByClassName("btn_modalSair_f")[0];
+
+  
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+  
+  fechar_modal_sair.onclick = function() {
+    modal_sair.style.display = "none";
+  }
+
+  
 // modal
 
 // Get the modal
-var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn_modal");
+// var btn = document.getElementById("btn_modal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("btn_modal_f")[0];
+// var span = document.getElementsByClassName("btn_modal_f")[0];
+
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
